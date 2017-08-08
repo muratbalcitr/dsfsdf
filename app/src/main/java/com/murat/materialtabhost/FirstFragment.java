54368eeda827541c4ea7a8be481da67aa262d8ee
muratbalcitr <Md260914>
2017-08-08 21:26:35 +0300

    first commit

---
+++ app/src/main/java/com/murat/materialtabhost/FirstFragment.java
@@ -0,0 +1,29 @@
+package com.murat.materialtabhost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by murat on 8.08.2017.
+ */
+
+public class FirstFragment extends Fragment {
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.first_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/murat/materialtabhost/Third_Fragment.java
@@ -0,0 +1,29 @@
+package com.murat.materialtabhost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by murat on 8.08.2017.
+ */
+
+public class Third_Fragment extends Fragment {
+    public Third_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.third_fragment, container, false);
+    }
+}
